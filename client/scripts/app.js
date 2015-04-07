452a6fa9cc78b1e9f092c6951f4c3a6940231da1
riahk <moar.riah@gmail.com>
2015-04-06 17:40:06 -0700

    added rooms object

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-
-
+  var currentRoom = 'lobby';
+  var rooms = {};
 
 
   var getMessages = function(){
@@ -9,9 +9,30 @@ $(document).ready(function(){
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'GET',
       contentType: 'application/json',
-      data:'order=-createdAt&limit=20',
+      data:'order=-createdAt&limit=20&where={"roomname":"'+currentRoom+'"}',
       success: function (data) {
         displayMessages(data);
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to get message');
+      }
+    });
+  }
+
+  var populateRooms = function(){
+    $.ajax({
+      // always use this url
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      contentType: 'application/json',
+      data:'order=-createdAt&limit=100',
+      success: function (data) {
+        _.each(data.results, function(obj){
+          if(obj.roomname){
+            rooms[obj.roomname] = obj.roomname;
+          }
+        });
       },
       error: function (data) {
         // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -23,9 +44,15 @@ $(document).ready(function(){
   var displayMessages = function(data){
     var messages = data.results;
     $(".chat").html('');
+
+
     _.each(messages, function(obj){
       //username :  text
       if(obj.text && obj.username) {
+        if(/<script>/.test(obj.text)){
+          obj.text = "GTFO Hacker!!!"
+        }
+
         var $message = '<p>'+obj.username+': '+obj.text+'</p>';
         $('.chat').append($message);
       }
@@ -39,7 +66,7 @@ $(document).ready(function(){
     var message = {
       'username': window.location.search.split('=')[1],
       'text': $('#chatbox').val(),
-      'roomname': 'lobby'
+      'roomname': currentRoom
     };
 
     $('#chatbox').val('');
@@ -62,15 +89,17 @@ $(document).ready(function(){
     });
   }
 
+
+
+
   $('#submit').on('click', function(event) {
-    // event.stopPropogation();
     event.preventDefault();
     postMessages();
   });
 
 
-
-
   getMessages();
-
+  populateRooms();
+  console.log(rooms)
+  setInterval(getMessages, 15000);
 });
